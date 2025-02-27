e3907524d1af965d3f14862422c4dbb1502ee9ab
Ioannis G. Tsoulos <itsoulos@gmail.com>
2025-02-27 07:30:25 +0200

    Add astika datasets in xlsx

---
+++ PAPER_WEATHER/DATA_FILES/normalize.cc
@@ -0,0 +1,64 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <vector>
+using namespace std;
+int main(int argc,char **argv)
+{
+	vector<double> min;
+	vector<double> max;
+	double ymin,ymax;
+	FILE *fp=fopen(argv[1],"r");
+	if(!fp) return 1;
+	int d,count;
+	fscanf(fp,"%d",&d);
+	fscanf(fp,"%d",&count);
+	min.resize(d);
+	max.resize(d);
+	for(int i=0;i<count;i++)
+	{
+		for(int j=0;j<d;j++)
+		{
+			double x;
+			fscanf(fp,"%lf",&x);
+			if(i==0) 
+			{
+				min[j]=x;
+				max[j]=x;
+			}	
+			if(x<min[j]) min[j]=x;
+			if(x>max[j]) max[j]=x;
+		}
+		double y;
+		fscanf(fp,"%lf",&y);
+		if(i==0) {ymin=y;ymax=y;}
+		if(y<ymin) ymin=y;
+		if(y>ymax) ymax=y;
+	}
+	fclose(fp);
+
+	
+	fp=fopen(argv[1],"r");
+	fscanf(fp,"%d",&d);
+	fscanf(fp,"%d",&count);
+	printf("%d\n%d\n",d,count);
+	for(int i=0;i<count;i++)
+	{
+		vector<double> xx;
+		xx.resize(d);
+		for(int j=0;j<d;j++)
+		{
+			double x;
+			fscanf(fp,"%lf",&x);
+			printf("%lf ",x);
+			x=(x-min[j])/(max[j]-min[j]);
+		}
+		double y;
+		fscanf(fp,"%lf",&y);
+		y = (ymax-y)/(ymax-ymin);
+		printf("%lf\n",y);
+	}
+	fclose(fp);
+
+	return 0;
+}
+
