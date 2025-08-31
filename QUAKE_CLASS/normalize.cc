fd70b2e278a45e49ce74592a00583f1c43c92672
Ioannis G. Tsoulos <itsoulos@gmail.com>
2025-08-31 20:13:13 +0300

    gct data

---
+++ QUAKE_CLASS/normalize.cc
@@ -0,0 +1,64 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <vector>
+using namespace std;
+int main(int argc,char **argv)
+{
+	vector<double> min;
+	vector<double> max;
+	double ymin,ymax;
+	FILE *fp=fopen(argv[1],"r");
+	if(!fp) return 1;
+	int d,count;
+	fscanf(fp,"%d",&d);
+	fscanf(fp,"%d",&count);
+	min.resize(d);
+	max.resize(d);
+	for(int i=0;i<count;i++)
+	{
+		for(int j=0;j<d;j++)
+		{
+			double x;
+			fscanf(fp,"%lf",&x);
+			if(i==0) 
+			{
+				min[j]=x;
+				max[j]=x;
+			}	
+			if(x<min[j]) min[j]=x;
+			if(x>max[j]) max[j]=x;
+		}
+		double y;
+		fscanf(fp,"%lf",&y);
+		if(i==0) {ymin=y;ymax=y;}
+		if(y<ymin) ymin=y;
+		if(y>ymax) ymax=y;
+	}
+	fclose(fp);
+
+	
+	fp=fopen(argv[1],"r");
+	fscanf(fp,"%d",&d);
+	fscanf(fp,"%d",&count);
+	printf("%d\n%d\n",d,count);
+	for(int i=0;i<count;i++)
+	{
+		for(int j=0;j<d;j++)
+		{
+			double x;
+			fscanf(fp,"%lf",&x);
+			//x=(x-min[j])/(max[j]-min[j]);
+			printf("%lf ",x);
+		}
+		double y;
+		fscanf(fp,"%lf",&y);
+		if(y<=1) continue;
+		if(y>4) y=1; else y=0;
+		//y=(y-ymin)/(ymax-ymin);
+		printf("%lf\n",y);
+	}
+	fclose(fp);
+
+	return 0;
+}
+
